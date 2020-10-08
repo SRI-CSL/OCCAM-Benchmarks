f48aa62b5770229c73118f2caabf4346f8818555
shoaibCS <19100080@lums.edu.pk>
2020-10-08 15:52:42 +0000

    Added Makefiles for applications in trimmer set

---
+++ examples/trimmer/netstat/build.sh
@@ -0,0 +1,128 @@
+#!/usr/bin/env bash
+
+# Make sure we exit if there is a failure
+set -e
+
+function usage() {
+    echo "Usage: $0 [--with-musllvm] [--disable-inlining] [--ipdse] [--ai-dce] [--use-pointer-analysis] [--inter-spec VAL] [--intra-spec VAL] [--enable-config-prime] [--help]"
+    echo "       VAL=none|aggressive|nonrec-aggressive|onlyonce"
+}
+
+#default values
+INTER_SPEC="none"
+INTRA_SPEC="onlyonce"
+USE_MUSLLVM="false"
+OPT_OPTIONS=""
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -inter-spec|--inter-spec)
+	INTER_SPEC="$2"
+	shift # past argument
+	shift # past value
+	;;
+    -intra-spec|--intra-spec)
+	INTRA_SPEC="$2"
+	shift # past argument
+	shift # past value
+	;;
+    -disable-inlining|--disable-inlining)
+	OPT_OPTIONS="${OPT_OPTIONS} --disable-inlining"
+	shift # past argument
+	;;
+    -enable-config-prime|--enable-config-prime)
+	OPT_OPTIONS="${OPT_OPTIONS} --enable-config-prime"
+	shift # past argument
+	;;
+    -with-musllvm|--with-musllvm)
+	USE_MUSLLVM="true" 
+	shift # past argument
+	;;            
+    -ipdse|--ipdse)
+	OPT_OPTIONS="${OPT_OPTIONS} --ipdse"
+	shift # past argument
+	;;
+    -ai-dce|--ai-dce)
+	OPT_OPTIONS="${OPT_OPTIONS} --ai-dce"
+	shift # past argument
+	;;
+    -use-pointer-analysis|--use-pointer-analysis)
+	OPT_OPTIONS="${OPT_OPTIONS} --use-pointer-analysis"	
+	shift # past argument
+	;;    
+    -help|--help)
+	usage
+	exit 0
+	;;
+    *)    # unknown option
+	POSITIONAL+=("$1") # save it in an array for later
+	shift # past argument
+	;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+#check that the require dependencies are built
+if [ $USE_MUSLLVM == "true" ];
+then
+    declare -a bitcode=("netstat.bc" "libc.a.bc" "libc.a")
+else
+    declare -a bitcode=("netstat.bc")
+fi    
+
+for bc in "${bitcode[@]}"
+do
+    if [ -a  "$bc" ]
+    then
+        echo "Found $bc"
+    else
+	if [ "$bc" == "libc.a.bc" ];
+	then
+	    echo "Error: $bc not found. You need to compile musllvm and copy $bc to ${PWD}."
+	else
+            echo "Error: $bc not found. Try \"make\"."
+	fi
+        exit 1
+    fi
+done
+
+
+MANIFEST=netstat.manifest
+
+cat > ${MANIFEST} <<EOF    
+{"binary": "netstat_fin", 
+"native_libs": [], 
+"name": "netstat", 
+"static_args": ["-a", "-e","-p","-s"], 
+"modules": [], 
+"ldflags": ["-O3"], 
+"main": "netstat.bc"}
+EOF
+
+    
+
+export OCCAM_LOGLEVEL=INFO
+export OCCAM_LOGFILE=${PWD}/slash/occam.log
+
+rm -rf slash
+
+# OCCAM
+SLASH_OPTS="--inter-spec-policy=${INTER_SPEC} --intra-spec-policy=${INTRA_SPEC}   --stats $OPT_OPTIONS"
+echo "============================================================"
+echo "Running with options ${SLASH_OPTS}"
+echo "============================================================"
+slash ${SLASH_OPTS} --work-dir=slash ${MANIFEST}
+
+status=$?
+if [ $status -eq 0 ]
+then
+    cp slash/netstat_fin netstat_slashed
+    strip slash/netstat_fin -o slash/netstat_fin_stripped
+else
+    echo "Something failed while running slash"
+fi    
+
+
